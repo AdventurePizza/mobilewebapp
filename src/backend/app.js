0c1f3a9bf069828b31fdba34dba64c9953988c11
Sharat Gajulapalli <blitzkreig@gmail.com>
2017-12-24 23:33:51 -0500

    build folder

--- src/backend/app.js
+++ src/backend/app.js
@@ -5,7 +5,9 @@ var app = express();
 
 var port = (process.env.PORT || 5000);
 
-server = app.listen(port, 'http://104.236.66.20:5000/');
+server = app.listen(port, function(){
+    console.log('server is running on port ' + port)
+});
 
 io = socket(server);
 
